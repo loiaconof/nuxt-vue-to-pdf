e6e37773fb276b9d0277f7830ff22dc660545a93
loiaconof <francesco.loiacono@linksmt.it>
2024-10-20 08:21:50 +0200

    update merged options inside exportVueToPdf functions

--- src/runtime/server/utils/export-vue-to-pdf.ts
+++ src/runtime/server/utils/export-vue-to-pdf.ts
@@ -3,7 +3,7 @@ import puppeteer, { type PDFOptions, type PuppeteerLaunchOptions } from 'puppete
 import { type Component, createSSRApp, h } from 'vue'
 import { renderToString } from 'vue/server-renderer'
 import type { H3Event } from 'h3'
-import { createError } from '#imports'
+import { createError, useRuntimeConfig } from '#imports'
 import type { VueToPdfOptions } from '~/src/types'
 
 const defaultPuppeteerLaunchOptions: PuppeteerLaunchOptions = {
@@ -17,8 +17,8 @@ const defaultPdfOptions: PDFOptions = {
 }
 
 export async function exportVueToPdf(event: H3Event, filename: string, component: Component, options?: Partial<VueToPdfOptions>) {
-  const mergedLaunchOptions = defu(options?.puppeteerLaunchOptions ?? {}, defaultPuppeteerLaunchOptions)
-  const mergedPdfOptions = defu(options?.pdfOptions ?? {}, defaultPdfOptions)
+  const mergedLaunchOptions = defu(options?.puppeteerLaunchOptions ?? {}, useRuntimeConfig().nuxtVueToPdf?.puppeteerLaunchOptions ?? {}, defaultPuppeteerLaunchOptions)
+  const mergedPdfOptions = defu(options?.pdfOptions ?? {}, useRuntimeConfig().nuxtVueToPdf?.pdfOptions ?? {}, defaultPdfOptions)
 
   const app = createSSRApp({
     render() {
