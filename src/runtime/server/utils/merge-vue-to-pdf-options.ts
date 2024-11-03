a38476c1635ba27a80f8d17c046783cc0fd32425
loiaconof <francescoloiacono95@outlook.it>
2024-11-03 19:10:59 +0100

    new utility to encapsulate merging options logic

---
+++ src/runtime/server/utils/merge-vue-to-pdf-options.ts
@@ -0,0 +1,51 @@
+import defu from 'defu'
+import { type H3Event } from 'h3'
+import type { VueToPdfOptions } from '~/src/types'
+import { getQuery, readBody, useRuntimeConfig } from '#imports'
+
+export async function mergeVueToPdfOptions(event: H3Event, options?: Partial<VueToPdfOptions>): VueToPdfOptions {
+  const defaultOptions: Readonly<VueToPdfOptions> = {
+    pdfOptions: {
+      format: 'A4',
+      printBackground: true,
+    },
+    puppeteerLaunchOptions: {
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    },
+    css: {
+      external: {
+        cdns: [],
+      },
+      local: [],
+    },
+    component: {
+      forwardRequest: true,
+      props: {},
+    },
+  }
+
+  const _options = defu(options, useRuntimeConfig().nuxtVueToPdf, defaultOptions) as VueToPdfOptions
+
+  if (!_options.component.forwardRequest)
+    return _options
+
+  const { props } = _options.component
+
+  if (props?.header) {
+    console.warn(`options.component.props.header will be overwritten due to forwardRequest flag`)
+  }
+  props.header = event.node.req.headers
+
+  if (props?.query) {
+    console.warn(`options.component.props.query will be overwritten due to forwardRequest flag`)
+  }
+  props.query = getQuery(event)
+
+  if (props?.body) {
+    console.warn(`options.component.props.body will be overwritten due to forwardRequest flag`)
+  }
+  props.body = event.node.req.method !== 'GET' ? await readBody(event) : undefined
+
+  return _options
+}
