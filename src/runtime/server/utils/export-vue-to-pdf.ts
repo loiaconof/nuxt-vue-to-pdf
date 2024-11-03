8f19073673aac6a583311bfd6730f50a79ded85d
loiaconof <francescoloiacono95@outlook.it>
2024-11-03 19:11:31 +0100

    aligned code to new utility

--- src/runtime/server/utils/export-vue-to-pdf.ts
+++ src/runtime/server/utils/export-vue-to-pdf.ts
@@ -1,41 +1,14 @@
-import { defu } from 'defu'
 import puppeteer from 'puppeteer'
 import { type Component, createSSRApp, h } from 'vue'
 import { renderToString } from 'vue/server-renderer'
-import type { H3Event } from 'h3'
+import { type H3Event } from 'h3'
 import { processLocalCss } from './process-local-css'
-import { createError, getQuery, readBody, useRuntimeConfig } from '#imports'
+import { mergeVueToPdfOptions } from './merge-vue-to-pdf-options'
+import { createError } from '#imports'
 import type { VueToPdfOptions } from '~/src/types'
 
-const defaultOptions: VueToPdfOptions = {
-  pdfOptions: {
-    format: 'A4',
-    printBackground: true,
-  },
-  puppeteerLaunchOptions: {
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  },
-  css: {
-    external: {
-      cdns: [],
-    },
-    local: [],
-  },
-  component: {
-    forwardRequest: true,
-    props: {},
-  },
-}
-
 export async function exportVueToPdf(event: H3Event, filename: string, component: Component, options?: Partial<VueToPdfOptions>) {
-  const _options = defu(options, useRuntimeConfig().nuxtVueToPdf, defaultOptions) as VueToPdfOptions
-
-  if (_options.component.forwardRequest) {
-    _options.component.props.header = event.node.req.headers
-    _options.component.props.query = getQuery(event)
-    _options.component.props.header = await readBody(event)
-  }
+  const _options = await mergeVueToPdfOptions(event, options)
 
   const app = createSSRApp({
     render() {
@@ -61,7 +34,9 @@ export async function exportVueToPdf(event: H3Event, filename: string, component
   let browser
   try {
     browser = await puppeteer.launch(_options.puppeteerLaunchOptions)
+
     const page = await browser.newPage()
+
     await page.setContent(html, { waitUntil: 'networkidle0' })
 
     const pdfBuffer = await page.pdf(_options.pdfOptions)
